af17d0eeb805d5bcc3351a03283f41345fdbccc6
Zhili Yang <zhya1033@colorado.edu>
2014-03-30 19:48:41 -0600

    init commit

---
+++ Gemfile
@@ -0,0 +1,24 @@
+source 'https://rubygems.org'
+gem 'rails', '3.2.13'
+ruby '1.9.3'
+group :development, :test do
+ gem 'sqlite3'
+ gem 'rspec-rails', '~> 3.0.0.beta'
+end
+group :test do
+ gem 'selenium-webdriver'
+ gem 'capybara'
+end
+gem 'sass-rails'
+gem 'uglifier'
+gem 'coffee-rails'
+gem 'jquery-rails'
+gem 'turbolinks'
+gem 'jbuilder'
+group :doc do
+ gem 'sdoc', '0.3.20', require: false
+end
+group :production do
+ gem 'pg'
+ gem 'rails_12factor'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'fb09c0bd62ac415cf31fe6fcdb7f35701ad5fc876ba051699abe40a04246651a8c8abbdd83c9f08d0cc76615df269e7401e5ed3eddebc342d0504e18281fe14f'
